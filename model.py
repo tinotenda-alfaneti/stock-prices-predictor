4efcbe8a0cfd9e748d05a60e8d5c08c64a523114
tinotenda-alfaneti <78873628+tinotenda-alfaneti@users.noreply.github.com>
2022-11-06 23:08:45 +0000

    Refactor Code

--- model.py
+++ model.py
@@ -8,91 +8,86 @@ from sklearn.preprocessing import MinMaxScaler
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout, LSTM
 
-#Loading Data
-company = 'AAPL'
 
-start = dt.datetime(2010,1,1)
-end = dt.datetime(2020,1,1)
+def train_model(model, prediction_days, company):
 
-data = web.DataReader(company, 'yahoo', start, end)
+    start = dt.datetime(2010,1,1)
+    end = dt.datetime.now()
 
-#Prepare Data
-scaler = MinMaxScaler(feature_range=(0,1)) #change all the values to be between 0 and 1
-scaled_data = scaler.fit_transform(data['Close'].values.reshape(-1,1)) #reshape the data to be 2D
-#only predict the closing price
+    data = web.DataReader(company, 'yahoo', start, end)
 
-prediction_days = 60 #how many days we want to use to predict the next day
+    #Prepare Data
+    scaler = MinMaxScaler(feature_range=(0,1)) #change all the values to be between 0 and 1
+    scaled_data = scaler.fit_transform(data['Close'].values.reshape(-1,1)) #reshape the data to be 2D
+    #only predict the closing price
 
+    #training data
+    x_train = []
+    y_train = []
 
-#training data
-x_train = []
-y_train = []
+    #fill up the x_train and y_train
+    for x in range(prediction_days, len(scaled_data)):
+        x_train.append(scaled_data[x-prediction_days:x, 0])
+        y_train.append(scaled_data[x, 0])
 
-#fill up the x_train and y_train
-for x in range(prediction_days, len(scaled_data)):
-    x_train.append(scaled_data[x-prediction_days:x, 0])
-    y_train.append(scaled_data[x, 0])
+    #convert to numpy arrays
+    x_train, y_train = np.array(x_train), np.array(y_train)
 
-#convert to numpy arrays
-x_train, y_train = np.array(x_train), np.array(y_train)
+    #reshape the data
+    x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
 
-#reshape the data
-x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
+    #add the first layer
+    model.add(LSTM(units=50, return_sequences=True, input_shape=(x_train.shape[1], 1)))
+    model.add(Dropout(0.2))
 
-#Build the model
-model = Sequential()
+    #add the second layer
+    model.add(LSTM(units=50, return_sequences=True))
+    model.add(Dropout(0.2))
 
-#add the first layer
-model.add(LSTM(units=50, return_sequences=True, input_shape=(x_train.shape[1], 1)))
-model.add(Dropout(0.2))
+    #add the third layer
+    model.add(LSTM(units=50))
+    model.add(Dropout(0.2))
 
-#add the second layer
-model.add(LSTM(units=50, return_sequences=True))
-model.add(Dropout(0.2))
+    model.add(Dense(25))
+    #add the output layer
+    model.add(Dense(units=1)) #prediction of the next closing value
 
-#add the third layer
-model.add(LSTM(units=50))
-model.add(Dropout(0.2))
+    #compile the model
+    model.compile(optimizer='adam', loss='mean_squared_error')
+    model.fit(x_train, y_train, epochs=25, batch_size=32)
 
-model.add(Dense(25))
-#add the output layer
-model.add(Dense(units=1)) #prediction of the next closing value
+def predict_next_day(selected_stock, data):
+    model = Sequential()
+    train_model(model, 60, selected_stock)
 
-#compile the model
-model.compile(optimizer='adam', loss='mean_squared_error')
-model.fit(x_train, y_train, epochs=25, batch_size=32)
+    #Load the test data 
+    test_start = dt.datetime(2020,1,1)
+    test_end = dt.datetime.now()
+    scaler = MinMaxScaler(feature_range=(0,1)) #change all the values to be between 0 and 1
+    
+    prediction_days = 60 #how many days we want to use to predict the next day
 
-#Testing the model
-#Load the test data - how it would perfom on past data
-test_start = dt.datetime(2020,1,1)
-test_end = dt.datetime(2023,1,1)
+    #load the data
+    test_data = web.DataReader(selected_stock, 'yahoo', test_start, test_end)
 
-#load the data
-test_data = web.DataReader(company, 'yahoo', test_start, test_end)
-actual_prices = test_data['Close'].values
+    #Get the predicted prices
+    total_dataset = pd.concat((data['Close'], test_data['Close']), axis=0)
 
-#Get the predicted prices
-total_dataset = pd.concat((data['Close'], test_data['Close']), axis=0)
+    #model inputs
+    model_inputs = total_dataset[len(total_dataset) - len(test_data) - prediction_days:].values
 
-#model inputs
-model_inputs = total_dataset[len(total_dataset) - len(test_data) - prediction_days:].values
+    #reshape the data
+    model_inputs = model_inputs.reshape(-1,1)
 
-#reshape the data
-model_inputs = model_inputs.reshape(-1,1)
+    #scale the data
+    model_inputs = scaler.transform(model_inputs)
 
-#scale the data
-model_inputs = scaler.transform(model_inputs)
+    #Predict Next Day
+    real_data = [model_inputs[len(model_inputs) + 1 - prediction_days:len(model_inputs+1), 0]]
+    real_data = np.array(real_data)
+    real_data = np.reshape(real_data, (real_data.shape[0], real_data.shape[1], 1))
 
-#Predict Next Day
-real_data = [model_inputs[len(model_inputs) + 1 - prediction_days:len(model_inputs+1), 0]]
-real_data = np.array(real_data)
-real_data = np.reshape(real_data, (real_data.shape[0], real_data.shape[1], 1))
-
-#Get the predicted price
-prediction = model.predict(real_data)
-prediction = scaler.inverse_transform(prediction)
-print(f"Prediction: {prediction}")
-
-#TODO: Predict the next 30 days
-
-
+    #Get the predicted price
+    prediction = model.predict(real_data)
+    prediction = scaler.inverse_transform(prediction)
+    return prediction[0][0]
--- webapp.py
+++ webapp.py
@@ -3,18 +3,10 @@ import pandas_datareader as web
 import matplotlib.pyplot as plt
 from plotly import graph_objs as go
 from datetime import datetime
-import numpy as np
-import matplotlib.pyplot as plt
-import pandas as pd
-import pandas_datareader as web
-import datetime as dt
-
-from sklearn.preprocessing import MinMaxScaler
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Dropout, LSTM
+from model import predict_next_day
 
 st.title('Atarist Stock Price Prediction')
-stocks = ('AAPL', 'GOOG', 'MSFT', 'AMZN')
+stocks = ('AAPL', 'GOOG', 'MSFT', 'AMZN', 'TSLA', 'JPM', 'META')
 selected_stock = st.selectbox('SELECT STOCK', stocks)
 
 @st.cache(persist=True)
@@ -37,109 +29,12 @@ def plot_raw_data():
 
 plot_raw_data()
 
-def train_model(model, prediction_days):
-    #Loading Data
-    company = selected_stock
-
-    start = dt.datetime(2010,1,1)
-    end = dt.datetime.now()
-
-    data = web.DataReader(company, 'yahoo', start, end)
-
-    #Prepare Data
-    scaler = MinMaxScaler(feature_range=(0,1)) #change all the values to be between 0 and 1
-    scaled_data = scaler.fit_transform(data['Close'].values.reshape(-1,1)) #reshape the data to be 2D
-    #only predict the closing price
-
-    #training data
-    x_train = []
-    y_train = []
-
-    #fill up the x_train and y_train
-    for x in range(prediction_days, len(scaled_data)):
-        x_train.append(scaled_data[x-prediction_days:x, 0])
-        y_train.append(scaled_data[x, 0])
-
-    #convert to numpy arrays
-    x_train, y_train = np.array(x_train), np.array(y_train)
-
-    #reshape the data
-    x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
-
-    #add the first layer
-    model.add(LSTM(units=50, return_sequences=True, input_shape=(x_train.shape[1], 1)))
-    model.add(Dropout(0.2))
-
-    #add the second layer
-    model.add(LSTM(units=50, return_sequences=True))
-    model.add(Dropout(0.2))
-
-    #add the third layer
-    model.add(LSTM(units=50))
-    model.add(Dropout(0.2))
-
-    model.add(Dense(25))
-    #add the output layer
-    model.add(Dense(units=1)) #prediction of the next closing value
-
-    #compile the model
-    model.compile(optimizer='adam', loss='mean_squared_error')
-    model.fit(x_train, y_train, epochs=25, batch_size=32)
-
 if st.button(f"PREDICT {selected_stock} NEXT DAY CLOSING PRICE"):
     st.write("Predicting...")
-    #Build the model
-    model = Sequential()
-    train_model(model, 60)
-
-    #Testing the model
-    #Load the test data - how it would perfom on past data
-    test_start = dt.datetime(2020,1,1)
-    test_end = dt.datetime.now()
-    scaler = MinMaxScaler(feature_range=(0,1)) #change all the values to be between 0 and 1
-    scaled_data = scaler.fit_transform(data['Close'].values.reshape(-1,1)) #reshape the data to be 2D
-    #only predict the closing price
-    prediction_days = 60 #how many days we want to use to predict the next day
-
-    #load the data
-    test_data = web.DataReader(selected_stock, 'yahoo', test_start, test_end)
-    actual_prices = test_data['Close'].values
-
-    #Get the predicted prices
-    total_dataset = pd.concat((data['Close'], test_data['Close']), axis=0)
-
-    #model inputs
-    model_inputs = total_dataset[len(total_dataset) - len(test_data) - prediction_days:].values
-
-    #reshape the data
-    model_inputs = model_inputs.reshape(-1,1)
-
-    #scale the data
-    model_inputs = scaler.transform(model_inputs)
-
-    #make predictions on test data
-    x_test = []
-
-    for x in range(prediction_days, len(model_inputs)):
-        x_test.append(model_inputs[x-prediction_days:x, 0])
-
-    #convert to numpy array
-    x_test = np.array(x_test)
-    x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
-
-    #Get the predicted prices
-    predicted_prices = model.predict(x_test)
-    predicted_prices = scaler.inverse_transform(predicted_prices)
-
-    #Predict Next Day
-    real_data = [model_inputs[len(model_inputs) + 1 - prediction_days:len(model_inputs+1), 0]]
-    real_data = np.array(real_data)
-    real_data = np.reshape(real_data, (real_data.shape[0], real_data.shape[1], 1))
-
-    #Get the predicted price
-    prediction = model.predict(real_data)
-    prediction = scaler.inverse_transform(prediction)
-    st.write(f"NEXT DAY'S PREDICTED CLOSING PRICE: {prediction[0][0]}")
+    
+    prediction_price = predict_next_day(selected_stock, data)
+    
+    st.write(f"NEXT DAY'S PREDICTED CLOSING PRICE: {prediction_price}")
 
 
 
